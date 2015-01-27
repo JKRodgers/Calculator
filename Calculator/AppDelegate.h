b931b0be55faaabac4e817552981867bf39544b9
Joshua Kyle Rodgers <jkr332@mcintosh.cs.mun.ca>
2015-01-27 16:17:08 -0330

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Joshua Kyle Rodgers on 2015-01-27.
+//  Copyright (c) 2015 Joshua Kyle Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Joshua Kyle Rodgers on 2015-01-27.
+//  Copyright (c) 2015 Joshua Kyle Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
